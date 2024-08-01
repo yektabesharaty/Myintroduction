d524c9cf53c489fb3ec8057dd50dda5abe7d2daa
yekta <yektabesharaty@gmail.com>
2024-08-02 01:14:42 +0330

    add introduction project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./styles.css";
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Photo />
+      <div className="data">
+        <Description />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+function Photo() {
+  return <img className="avatar" src="my.jpg" alt="yekta" />;
+}
+function Description() {
+  return (
+    <div>
+      <h1>Yekta Besharaty</h1>
+      <p>
+        Gaining skills and experience to become a front-end programmer and
+        computer student
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="👶" color="#FF8C9E" />
+      <Skill skill="HTML+CSS" emoji="💪" color="#DC0083" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="bootstrap" emoji="💪" color="#3DC2EC" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ background: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
